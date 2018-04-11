5f78639216f8eb6cc3dd4da103078b974cd49f76
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-04-11 20:44:03 +0200

    move notification generation to plugins

---
+++ backend/Notification.js
@@ -0,0 +1,14 @@
+/**
+ * create Notification from message
+ *
+ * @author Tobias Bräutigam <tbraeutigam@gmail.com>
+ * @since 2018
+ */
+const createNotification = (message) => {
+  return {
+    phrase: 'New message in %s',
+    content: message.content.message
+  }
+}
+
+module.exports = createNotification
